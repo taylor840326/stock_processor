cd8edbf1eb2b8ef66278520e01affa9ccb43f0bf
Tian Lei <553948327@qq.com>
2020-12-21 15:07:15 +0000

    init commit

---
+++ src/main/java/com/imsql/stock/StockApplication.java
@@ -0,0 +1,13 @@
+package com.imsql.stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockApplication.class, args);
+	}
+
+}
