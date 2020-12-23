e931f595ba5888c33b40e5a02b710d20942ca25a
Tian Lei <553948327@qq.com>
2020-12-23 00:08:47 +0000

    添加springboot-web以来

---
+++ src/main/java/com/imsql/stock/entity/Daily.java
@@ -0,0 +1,39 @@
+package com.imsql.stock.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "t_daily")
+public class Daily implements Serializable{
+    private String ts_code;
+    private String trade_date;
+    @Column(name = "open_price")
+    private Float open;
+    @Column(name = "high_price")
+    private Float high;
+    @Column(name = "low_price")
+    private Float low;
+    @Column(name = "close_price")
+    private Float close;
+    private Float pre_close;
+    @Column(name = "change_price")
+    private Float change;
+    private Float pct_chg;
+    private Float vol;
+    private Float amount;
+}
